748959d0af3967e225d6528bce7b273f362dfc46
Zakaria <zakaria.akli.ensa@gmail.com>
2020-07-28 21:30:09 +0200

    load speed management 5

--- components/Cards.js
+++ components/Cards.js
@@ -7,13 +7,13 @@ const Cards = props => {
         <div className="container-fluid d-flex justify-content-center">
             <div className="row">
                 <div className="col-md-4 col-sm-12">
-                    <Card img="/img/IMG_0950.jpg" imgAvatar="/img/avatar.jpg"/>
+                    <Card img="/img/IMG_0950.webp" imgAvatar="/img/avatar.webp"/>
                 </div>
                 <div className="col-md-4 col-sm-12">
-                    <Card img="/img/IMG_0914.jpg" imgAvatar="/img/avatar.jpg"/>
+                    <Card img="/img/IMG_0914.webp" imgAvatar="/img/avatar.webp"/>
                 </div>
                 <div className="col-md-4 col-sm-12">
-                    <Card img="/img/IMG_1088.jpg" imgAvatar="/img/avatar.jpg"/>
+                    <Card img="/img/IMG_1088.webp" imgAvatar="/img/avatar.webp"/>
                 </div>
 
             </div>
