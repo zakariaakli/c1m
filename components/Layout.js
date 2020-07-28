745172e617653989d0c09fdf629b558e1dd73a10
Zakaria <zakaria.akli.ensa@gmail.com>
2020-07-28 21:07:56 +0200

    load speed management 4

--- components/Layout.js
+++ components/Layout.js
@@ -21,7 +21,7 @@ const Layout = props => {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta charSet="utf-8" />
         <link rel="shortcut icon" href="/static/favicon.ico" />
-        <link rel="stylesheet" defer href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" />
+        <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="preload" as="style" onload="this.rel='stylesheet'"  />
         <script async src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
         <script async src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
       </Head>
