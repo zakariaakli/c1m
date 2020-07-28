2bb914befb0460fb0af85481a03c1c56175b5601
Zakaria <zakaria.akli.ensa@gmail.com>
2020-07-28 15:52:18 +0200

    amp thing

--- pages/explore.js
+++ pages/explore.js
@@ -1,10 +1,8 @@
 // pages/explore.js
 
 import Layout from "../components/Layout";
-import { useAmp } from 'next/amp'; 
 import Card from "../components/Card"; 
 
-export const config = { amp: 'hybrid' }
 
 const ExploreView = props => <Layout>
       
