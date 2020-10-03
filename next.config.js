5bcf1061b1fe7eb7330896b4f1d7fbda8a0fb90d
Zakaria <zakaria.akli.ensa@gmail.com>
2020-10-03 18:17:56 +0200

    add dynamic map.xml

--- next.config.js
+++ next.config.js
@@ -1,5 +1,12 @@
 // next.config.js
 
 const withSass = require("@zeit/next-sass");
-module.exports = withSass();
+module.exports = withSass({
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      require('./utils/generateSiteMap')
+    }
 
+    return config
+  }
+});
