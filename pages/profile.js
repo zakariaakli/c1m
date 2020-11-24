082bc596d5cd24018304a328f621548b475062c3
Mohamed AKLI <m.akli@spinevision.com>
2020-11-24 13:38:51 +0100

    adding dynamic date

--- pages/profile.js
+++ pages/profile.js
@@ -4,6 +4,7 @@ import SocialmediaBar from '../components/socialmediaBar';
 import Layout from "../components/Layout";
 import ArticleCards from "../components/ArticleCard"
 import Table from '../components/table'
+import { format, addMonths} from 'date-fns'
 
 const ProfileView = props => <Layout><div class="container"> 
   <div class="row">
@@ -16,7 +17,7 @@ const ProfileView = props => <Layout><div class="container">
                     title= "Histoire"
                     id= 'histoire-french-montana'
                     resume= 'This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.'
-                    update = "Updated at 3pm">
+                    update = {"Updated at " + format(addMonths(new Date(), 0), 'hh' + ' aa')}>
 
         </ArticleCards>
         </div>
@@ -26,7 +27,7 @@ const ProfileView = props => <Layout><div class="container">
                     title= "Echec"
                     id= 'histoire-french-montana'
                     resume= 'This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.'
-                    update = "Updated at 3pm" >
+                    update = {"Updated at " + format(addMonths(new Date(), 0), 'hh' + ' aa')} >
                     
 
         </ArticleCards>
@@ -39,7 +40,7 @@ const ProfileView = props => <Layout><div class="container">
                     title= "Affrontements"
                     id= 'histoire-french-montana'
                     resume= 'This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.'
-                    update = "Updated at 3pm">
+                    update = {"Updated at " + format(addMonths(new Date(), 0), 'hh' + ' aa')}>
 
         </ArticleCards>
         </div>
@@ -49,7 +50,7 @@ const ProfileView = props => <Layout><div class="container">
                     title= "Résultats"
                     id= 'histoire-french-montana'
                     resume= 'This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.'
-                    update = "Updated at 3pm">
+                    update = {"Updated at " + format(addMonths(new Date(), 0), 'hh' + ' aa')}>
 
         </ArticleCards>
         </div>
@@ -63,21 +64,21 @@ const ProfileView = props => <Layout><div class="container">
 </Layout>;
 
 
-const ProfileView = props =>
-  <Layout>
-    <div className="container">
-      <div className="row">
-        <SocialmediaBar />
-        <h2 style={{ marginLeft: '11%', marginTop: 70, fontWeight: "bold" }}>About Us</h2>
-        <Hr />
-        <div>
-          <p>
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-          </p>
-        </div>
-      </div>
-    </div>
-  </Layout>;
+// const ProfileView = props =>
+//   <Layout>
+//     <div className="container">
+//       <div className="row">
+//         <SocialmediaBar />
+//         <h2 style={{ marginLeft: '11%', marginTop: 70, fontWeight: "bold" }}>About Us</h2>
+//         <Hr />
+//         <div>
+//           <p>
+//             Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+//           </p>
+//         </div>
+//       </div>
+//     </div>
+//   </Layout>;
 
 
 export default ProfileView;
