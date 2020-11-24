b42707c26256addaf111ef7cfec1eae1c973aec2
Mohamed AKLI <m.akli@spinevision.com>
2020-11-24 17:18:20 +0100

    editing website design

--- components/carousel.js
+++ components/carousel.js
@@ -3,27 +3,36 @@ import './carousel.scss';
 const Carousel = props => {
 
     return(
-        <div id="carouselExampleControls" className="carousel slide mobileSlider" data-ride="carousel">
-  <div className="carousel-inner">
-    <div className="carousel-item active">
-      <img className="d-block w-100 responsive" src="/img/Welcome.gif" alt="First slide"/>
+      <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img class="d-block w-100" src="/img/Every child is an artist. The problem is how to remain an artist once we grow up.png" alt="First slide"/>
+          <div class="carousel-caption">
+            
+          </div>
+        </div>
+        <div class="carousel-item">
+          <img class="d-block w-100" src="/img/DAFT PUNK.gif" alt="Second slide"/>
+          <div class="carousel-caption bg-13">
+            
+          </div>
+        </div>
+        <div class="carousel-item">
+          <img class="d-block w-100" src="/img/FRENCH MONTANA.gif" alt="Third slide"/>
+          <div class="carousel-caption">
+            
+          </div>
+        </div>
+      </div>
+      <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+      </a>
     </div>
-    <div className="carousel-item">
-      <img className="d-block w-100 responsive" src="/img/Zaks trip.gif" alt="Second slide"/>
-    </div>
-    <div className="carousel-item">
-      <img className="d-block w-100 responsive" src="/img/Mo project.gif" alt="Third slide"/>
-    </div>
-  </div>
-  <a className="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span className="sr-only">Previous</span>
-  </a>
-  <a className="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-    <span className="carousel-control-next-icon" aria-hidden="true"></span>
-    <span className="sr-only">Next</span>
-  </a>
-</div>
     
     )
 }
