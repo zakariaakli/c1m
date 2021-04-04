210d12d3826d840b56eda829908383751e68422a
Zakaria <zakaria.akli.ensa@gmail.com>
2021-04-05 00:40:10 +0200

    correct syntax

--- components/aboutUs.js
+++ components/aboutUs.js
@@ -8,7 +8,7 @@ function aboutUs() {
           <div class="col-sm-8">
             <h2 class="stylefont">Jeune <br></br> artiste ?  </h2>
 
-            <h4 class="about-margin-text about-text">Rejoins GreenRoom et profite de notre offre d’accompagnement <strong>Yartist</strong>.<br/> <br/> Joue ta musique dans l’un de nos studios partenaires. Obtiens ton label <strong>Yartist</strong>. <br/> <br/> Bars, restaurants et entreprises membres de notre plateforme t’inviterons ensuite pour rajouter une touche artistique à leurs évènements.</h4>
+            <h4 class="about-margin-text about-text">Rejoins GreenRoom et profite de notre offre d’accompagnement <strong>Yartist</strong>.<br/> <br/> Joue ta musique dans l’un de nos studios partenaires. Obtiens ton label <strong>Yartist</strong>. <br/> <br/> Bars, restaurants et entreprises membres de notre plateforme t’inviterons ensuite pour rajouter une touche artistique à leurs événements.</h4>
             <br></br>
             <p>N’hésite plus pour devenir l’artiste que tu souhaites.</p>
             <br></br>
@@ -16,7 +16,7 @@ function aboutUs() {
           </div>
           <div class="col-sm-4">
             {/* <span class="fa fa-rss logo" style ={{fontSize : 200, color : "#494D5f"}}></span> */}
-            <img src="img/test3.png" style={{ width: 600, shadowColor: 'black', shadowOpacity: 1.0 }}></img>
+            <img src="img/test3.png" class = "responsive" style={{ shadowColor: 'black', shadowOpacity: 1.0 }}></img>
           </div>
         </div>
       </div>
@@ -24,7 +24,7 @@ function aboutUs() {
       <div class="container-fluid bg-light" >
         <div class="row">
           <div class="col-sm-4">
-            <img src="img/restau.png" style={{ width: 600, shadowColor: 'black', shadowOpacity: 1.0 }}></img>
+            <img src="img/restau.png" class = "responsive" style={{ shadowColor: 'black', shadowOpacity: 1.0 }}></img>
           </div>
           <div class="col-sm-8" >
             <h2 class="stylefont">Manager de <br></br> restaurant ? </h2>
